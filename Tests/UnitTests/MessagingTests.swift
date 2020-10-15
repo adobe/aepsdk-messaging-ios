c72fee23168f84d6e3b78ad66e37976777a3ee78
ravinsingh jain <ravjain@adobe.com>
2020-10-15 13:41:59 -0700

    Adding unit tests for messaging

--- Tests/UnitTests/MessagingTests.swift
+++ Tests/UnitTests/MessagingTests.swift
@@ -16,7 +16,62 @@ import XCTest
 @testable import AEPMessaging
 
 class MessagingTests: XCTestCase {
+    var messaging: Messaging!
+    var mockRuntime: TestableExtensionRuntime!
+
+    // before each
+    override func setUp() {
+        mockRuntime = TestableExtensionRuntime()
+        messaging = Messaging(runtime: mockRuntime)
+        messaging.onRegistered()
+    }
+
+    /// validate the extension is registered without any error
     func testregisterExtension_registersWithoutAnyErrorOrCrash() {
         XCTAssertNoThrow(MobileCore.registerExtensions([Messaging.self]))
+    }
+
+    /// validate that 3 listeners are registered onRegister
+    func testonRegistered_threeListenersAreRegistered() {
+        XCTAssertEqual(mockRuntime.listeners.count, 3)
+    }
+
+    /// validating handleConfigurationResponse does not throw error with nil data and
+    func testhandleConfigurationResponse_withNilData() {
+        let event: Event = Event(name: "configurationresponseEvent", type: MessagingConstants.EventTypes.configuration, source: MessagingConstants.EventSources.responseContent, data: nil)
+        XCTAssertNoThrow(messaging.handleConfigurationResponse(event))
+        XCTAssertFalse(mockRuntime.isStarted)
+        XCTAssertFalse(mockRuntime.isStopped)
+    }
+
+    /// validating handleConfigurationResponse when privacy is optedout
+    func testhandleConfigurationResponse_withPrivacyOptedOut() {
+        let eventData: [String: Any] = [MessagingConstants.SharedState.Configuration.privacyStatus: "optedout"]
+        let event: Event = Event(name: "configurationresponseEvent", type: MessagingConstants.EventTypes.configuration, source: MessagingConstants.EventSources.responseContent, data: eventData)
+        XCTAssertNoThrow(messaging.handleConfigurationResponse(event))
+        XCTAssertFalse(mockRuntime.isStarted)
+        XCTAssertTrue(mockRuntime.isStopped)
+    }
+
+    /// validating handleConfigurationResponse when privacy is optedIn
+    func testhandleConfigurationResponse_withPrivacyOptedIn() {
+        let eventData: [String: Any] = [MessagingConstants.SharedState.Configuration.privacyStatus: "optedin"]
+        let event: Event = Event(name: "configurationresponseEvent", type: MessagingConstants.EventTypes.configuration, source: MessagingConstants.EventSources.responseContent, data: eventData)
+        XCTAssertNoThrow(messaging.handleConfigurationResponse(event))
+        XCTAssertTrue(mockRuntime.isStarted)
+        XCTAssertFalse(mockRuntime.isStopped)
+    }
+
+    // validating handleProcessEvent withNilData
+    func testhandleProcessEvent_withNilEventData() {
+        let event: Event = Event(name: "handleProcessEvent", type: MessagingConstants.EventTypes.genericIdentity, source: MessagingConstants.EventSources.requestContent, data: nil)
+        XCTAssertNoThrow(messaging.handleProcessEvent(event))
+    }
+
+    // validating handleProcessEvent
+    func testhandleProcessEvent() {
+        let eventData: [String: Any] = [MessagingConstants.SharedState.Configuration.privacyStatus: "optedin"]
+        let event: Event = Event(name: "handleProcessEvent", type: MessagingConstants.EventTypes.genericIdentity, source: MessagingConstants.EventSources.requestContent, data: eventData)
+        XCTAssertNoThrow(messaging.handleProcessEvent(event))
     }
 }
