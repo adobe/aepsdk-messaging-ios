75f186b7991e41ca0d837bf9538448101c755a6f
ravinsingh jain <ravjain@adobe.com>
2021-04-26 18:15:45 -0700

    Updating app delegate

--- SampleApps/MessagingDemoApp/AppDelegate.swift
+++ SampleApps/MessagingDemoApp/AppDelegate.swift
@@ -40,9 +40,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         MobileCore.setLogLevel(.trace)
         MobileCore.registerExtensions([Lifecycle.self, Messaging.self, Edge.self, Signal.self, Identity.self])
 
-        MobileCore.configureWith(appId: "3149c49c3910/6a68c2e19c81/launch-4b2394565377-development")
+        MobileCore.configureWith(appId: "<appid>")
         MobileCore.updateConfigurationWith(configDict: [
-            "messaging.eventDataset": "5f8623492312f418dcc6b3d9",
+            "messaging.eventDataset": "<datasetid>",
             "messaging.useSandbox": true,
         ])
 
