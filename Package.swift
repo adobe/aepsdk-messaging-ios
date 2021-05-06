e60257fedcb594c05aa1ee89e96a32c3f525481d
ravinsingh jain <ravjain@adobe.com>
2021-05-06 14:23:21 -0700

    Remove unnecessary config

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 /*
- Copyright 2020 Adobe. All rights reserved.
+ Copyright 2021 Adobe. All rights reserved.
  This file is licensed to you under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License. You may obtain a copy
  of the License at http://www.apache.org/licenses/LICENSE-2.0
