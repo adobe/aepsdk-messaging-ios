41a38ca15b2a097a069de7a9bbaee3e4e7037fa0
Steve Benedick <sbenedic@adobe.com>
2022-09-15 14:14:21 -0600

    -better code

--- AEPMessaging/Sources/Messaging.swift
+++ AEPMessaging/Sources/Messaging.swift
@@ -200,16 +200,14 @@ public class Messaging: NSObject, Extension {
         }
 
         let message = Message(parent: self, event: event)
-        currentMessage = message
-        
-        if let propositionInfo = rulesEngine.propositionInfoForMessageId(message.id) {
-            currentMessage?.propositionInfo = propositionInfo
-        } else {
+        message.propositionInfo = rulesEngine.propositionInfoForMessageId(message.id)
+        if message.propositionInfo == nil {
             Log.warning(label: MessagingConstants.LOG_TAG, "Preparing to show a message that does not contain information necessary for tracking with Adobe Journey Optimizer. If you are spoofing this message from the AJO authoring UI or from Assurance, ignore this message.")
         }
 
-        currentMessage?.trigger()
-        currentMessage?.show()
+        message.trigger()
+        message.show()
+        currentMessage = message
     }
 
     // MARK: - Event Handers
