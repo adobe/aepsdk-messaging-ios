ba405968e5dd45e97409879aea541ff811cc998c
pravin <pravinpk0707@gmail.com>
2023-02-23 04:41:41 -0800

    DemoApp update to handle rich push notification

--- TestApps/MessagingDemoApp/ViewController.swift
+++ TestApps/MessagingDemoApp/ViewController.swift
@@ -17,14 +17,13 @@ import AEPAssurance
 import UIKit
 import UserNotifications
 import WebKit
+import UserNotifications
 
 class ViewController: UIViewController {
     @IBOutlet var switchShowMessages: UISwitch?
 
     private let messageHandler = MessageHandler()
     
-    weak var appDelegate = UIApplication.shared.delegate as? AppDelegate
-
     override func viewDidLoad() {
         super.viewDidLoad()
         MobileCore.messagingDelegate = messageHandler
@@ -119,11 +118,11 @@ class ViewController: UIViewController {
     }
 
     @IBAction func scheduleNotification(_: Any) {
-        appDelegate?.scheduleNotification()
+        scheduleNotification()
     }
 
     @IBAction func scheduleNotificationWithCustomAction(_: Any) {
-        appDelegate?.scheduleNotificationWithCustomAction()
+        scheduleNotificationWithCustomAction()
     }
 
     @IBAction func refreshMessages(_: Any) {
@@ -157,5 +156,67 @@ class ViewController: UIViewController {
     @IBAction func sendTrackAction(_: Any) {
         let data = ["key": "value"]
         MobileCore.track(action: "buttonClicked", data: data)
+    }
+    
+    func scheduleNotification() {
+        let content = UNMutableNotificationContent()
+
+        content.title = "Notification Title"
+        content.body = "This is example how to create "
+
+        // userInfo is mimicking data that would be provided in the push payload by Adobe Journey Optimizer
+        content.userInfo = ["_xdm": ["cjm": ["_experience": ["customerJourneyManagement":
+                                                                ["messageExecution": ["messageExecutionID": "16-Sept-postman", "messageID": "567",
+                                                                                      "journeyVersionID": "some-journeyVersionId", "journeyVersionInstanceId": "someJourneyVersionInstanceId"]]]]]]
+
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 2, repeats: false)
+        let identifier = "Local Notification"
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("Error \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func scheduleNotificationWithCustomAction() {
+        let content = UNMutableNotificationContent()
+
+        content.title = "Notification Title"
+        content.body = "This is example how to create "
+        content.categoryIdentifier = "MEETING_INVITATION"
+        // userInfo is mimicking data that would be provided in the push payload by Adobe Journey Optimizer
+        content.userInfo = ["_xdm": ["cjm": ["_experience": ["customerJourneyManagement":
+                                                                ["messageExecution": ["messageExecutionID": "16-Sept-postman", "messageID": "567",
+                                                                                      "journeyVersionID": "some-journeyVersionId", "journeyVersionInstanceId": "someJourneyVersionInstanceId"]]]]]]
+
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 2, repeats: false)
+        let identifier = "Local Notification"
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+
+        // Define the custom actions.
+        let acceptAction = UNNotificationAction(identifier: "ACCEPT_ACTION",
+                                                title: "Accept",
+                                                options: .foreground)
+        let declineAction = UNNotificationAction(identifier: "DECLINE_ACTION",
+                                                 title: "Decline",
+                                                 options: .destructive)
+        // Define the notification type
+        let meetingInviteCategory =
+            UNNotificationCategory(identifier: "MEETING_INVITATION",
+                                   actions: [acceptAction, declineAction],
+                                   intentIdentifiers: [],
+                                   hiddenPreviewsBodyPlaceholder: "",
+                                   options: .customDismissAction)
+        // Register the notification type.
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.setNotificationCategories([meetingInviteCategory])
+
+        notificationCenter.add(request) { error in
+            if let error = error {
+                print("Error \(error.localizedDescription)")
+            }
+        }
     }
 }
