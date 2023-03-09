dfec49c93a40bb2c3c140c7e625da42fcadfba00
Steve Benedick <sbenedic@adobe.com>
2023-03-09 14:03:20 -0700

    -rework test app and commit pods

---
+++ Pods/Target Support Files/Pods-E2EFunctionalTestApp/Pods-E2EFunctionalTestApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_E2EFunctionalTestApp : NSObject
+@end
+@implementation PodsDummy_Pods_E2EFunctionalTestApp
+@end
---
+++ Pods/Target Support Files/Pods-MessagingDemoApp/Pods-MessagingDemoApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MessagingDemoApp : NSObject
+@end
+@implementation PodsDummy_Pods_MessagingDemoApp
+@end
