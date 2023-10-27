cf38c2dc2d2c26540f2af660e79e0d17af20f683
timkimadobe <95260439+timkimadobe@users.noreply.github.com>
2023-10-27 14:44:38 -0700

    Refactor payloadObjectIsValid to use JSON comparison tool
    # Conflicts:
    #	AEPMessaging/Tests/FunctionalTests/InAppMessagingEventTests.swift

--- AEPMessaging/Tests/FunctionalTests/InAppMessagingEventTests.swift
+++ AEPMessaging/Tests/FunctionalTests/InAppMessagingEventTests.swift
@@ -200,209 +200,35 @@ class InAppMessagingEventTests: XCTestCase {
         DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(seconds), execute: closure)
     }
     
-    func payloadObjectIsValid(_ payload: [String: Any]) -> Bool {
-        
-        var objectIsValid = true
-                
-        /// {
-        ///     "id": "string",
-        ///     "scope": "string",
-        ///     "scopeDetails": {
-        ///         "activity": {
-        ///             "id": "string"
-        ///         },
-        ///         "characteristics": {
-        ///             "eventToken": "string"
-        ///         },
-        ///         "correlationID": "string",
-        ///         "decisionProvider": "string"
-        ///     },
-        ///     "items": [
-        ///         {
-        ///             "id": "string",
-        ///             "schema": "string",
-        ///             "data": {
-        ///                 "content": "string", // <<< sdk rule
-        ///                 "id": "string"
-        ///             }
-        ///         }
-        ///     ]
-        /// }
-        
-        // validate required fields are in first payload item and their types are correct
-        if !payload.contains(where: { $0.key == "id" }) {
-            XCTFail(self.missingField("id"))
-            objectIsValid = false
-        } else {
-            let value = payload["id"] as? String
-            if value == nil {
-                XCTFail(self.wrongType("id", expected: "String"))
-                objectIsValid = false
+    func validatePayloadObject(_ payload: [String: Any]) {
+        let expectedJSON = #"""
+        {
+          "id": "string",
+          "items": [
+            {
+              "data": {
+                "content": "string",
+                "id": "string"
+              },
+              "id": "string",
+              "schema": "string"
             }
+          ],
+          "scope": "string",
+          "scopeDetails": {
+            "activity": {
+              "id": "string"
+            },
+            "characteristics": {
+              "eventToken": "string"
+            },
+            "correlationID": "string",
+            "decisionProvider": "string"
+          }
         }
+        """#
         
-        if !payload.contains(where: { $0.key == "scope" }) {
-            XCTFail(self.missingField("scope"))
-            objectIsValid = false
-        } else {
-            let value = payload["scope"] as? String
-            if value == nil {
-                XCTFail(self.wrongType("scope", expected: "String"))
-                objectIsValid = false
-            }
-        }
-        
-        var scopeDetails: [String: Any]?
-        if !payload.contains(where: { $0.key == "scopeDetails" }) {
-            XCTFail(self.missingField("scopeDetails"))
-            objectIsValid = false
-        } else {
-            scopeDetails = payload["scopeDetails"] as? [String: Any]
-            if scopeDetails == nil {
-                XCTFail(self.wrongType("scopeDetails", expected: "[String: Any]"))
-                objectIsValid = false
-            }
-        }
-        
-        var activity: [String: Any]?
-        if !(scopeDetails?.contains(where: { $0.key == "activity" }) ?? false) {
-            XCTFail(self.missingField("scopeDetails.activity"))
-            objectIsValid = false
-        } else {
-            activity = scopeDetails?["activity"] as? [String: Any]
-            if activity == nil {
-                XCTFail(self.wrongType("scopeDetails.activity", expected: "[String: Any]"))
-                objectIsValid = false
-            }
-        }
-        
-        if !(activity?.contains(where: { $0.key == "id" }) ?? false) {
-            XCTFail(self.missingField("scopeDetails.activity.id"))
-            objectIsValid = false
-        } else {
-            let value = activity?["id"] as? String
-            if value == nil {
-                XCTFail(self.wrongType("scopeDetails.activity.id", expected: "String"))
-                objectIsValid = false
-            }
-        }
-        
-        var characteristics: [String: Any]?
-        if !(scopeDetails?.contains(where: { $0.key == "characteristics" }) ?? false) {
-            XCTFail(self.missingField("scopeDetails.characteristics"))
-            objectIsValid = false
-        } else {
-            characteristics = scopeDetails?["characteristics"] as? [String: Any]
-            if characteristics == nil {
-                XCTFail(self.wrongType("scopeDetails.characteristics", expected: "[String: Any]"))
-                objectIsValid = false
-            }
-        }
-        
-        if !(characteristics?.contains(where: { $0.key == "eventToken" }) ?? false) {
-            XCTFail(self.missingField("scopeDetails.characteristics.eventToken"))
-            objectIsValid = false
-        } else {
-            let value = characteristics?["eventToken"] as? String
-            if value == nil {
-                XCTFail(self.wrongType("scopeDetails.characteristics.eventToken", expected: "String"))
-                objectIsValid = false
-            }
-        }
-        
-        if !(scopeDetails?.contains(where: { $0.key == "correlationID" }) ?? false) {
-            XCTFail(self.missingField("scopeDetails.correlationID"))
-            objectIsValid = false
-        } else {
-            let value = scopeDetails?["correlationID"] as? String
-            if value == nil {
-                XCTFail(self.wrongType("scopeDetails.correlationID", expected: "String"))
-                objectIsValid = false
-            }
-        }
-        
-        if !(scopeDetails?.contains(where: { $0.key == "decisionProvider" }) ?? false) {
-            XCTFail(self.missingField("scopeDetails.decisionProvider"))
-            objectIsValid = false
-        } else {
-            let value = scopeDetails?["decisionProvider"] as? String
-            if value == nil {
-                XCTFail(self.wrongType("scopeDetails.decisionProvider", expected: "String"))
-                objectIsValid = false
-            }
-        }
-        
-        var items: [[String: Any]]?
-        if !payload.contains(where: { $0.key == "items" }) {
-            XCTFail(self.missingField("items"))
-            objectIsValid = false
-        } else {
-            items = payload["items"] as? [[String: Any]]
-            if items == nil {
-                XCTFail(self.wrongType("items", expected: "[[String: Any]]"))
-                objectIsValid = false
-            }
-        }
-        
-        let item = items?.first
-        
-        if !(item?.contains(where: { $0.key == "id" }) ?? false) {
-            XCTFail(self.missingField("items[0].id"))
-            objectIsValid = false
-        } else {
-            let value = item?["id"] as? String
-            if value == nil {
-                XCTFail(self.wrongType("items[0].id", expected: "String"))
-                objectIsValid = false
-            }
-        }
-        
-        if !(item?.contains(where: { $0.key == "schema" }) ?? false) {
-            XCTFail(self.missingField("items[0].schema"))
-            objectIsValid = false
-        } else {
-            let value = item?["schema"] as? String
-            if value == nil {
-                XCTFail(self.wrongType("items[0].schema", expected: "String"))
-                objectIsValid = false
-            }
-        }
-        
-        var itemData: [String: Any]?
-        if !(item?.contains(where: { $0.key == "data" }) ?? false) {
-            XCTFail(self.missingField("items[0].data"))
-            objectIsValid = false
-        } else {
-            itemData = item?["data"] as? [String: Any]
-            if itemData == nil {
-                XCTFail(self.wrongType("items[0].data", expected: "[String: Any]"))
-                objectIsValid = false
-            }
-        }
-        
-        if !(itemData?.contains(where: { $0.key == "content" }) ?? false) {
-            XCTFail(self.missingField("items[0].data.content"))
-            objectIsValid = false
-        } else {
-            let value = itemData?["content"] as? String
-            if value == nil {
-                XCTFail(self.wrongType("items[0].data.content", expected: "String"))
-                objectIsValid = false
-            }
-        }
-        
-        if !(itemData?.contains(where: { $0.key == "id" }) ?? false) {
-            XCTFail(self.missingField("items[0].data.id"))
-            objectIsValid = false
-        } else {
-            let value = itemData?["id"] as? String
-            if value == nil {
-                XCTFail(self.wrongType("items[0].data.id", expected: "String"))
-                objectIsValid = false
-            }
-        }
-        
-        return objectIsValid
+        assertTypeMatch(expected: getAnyCodable(expectedJSON)!, actual: AnyCodable(AnyCodable.from(dictionary: (payload))))
     }
     
     func missingField(_ key: String) -> String {
