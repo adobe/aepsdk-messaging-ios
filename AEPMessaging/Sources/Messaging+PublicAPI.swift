07105f145c2e7034802c298ceb915523e160f4ab
Steve Benedick <sbenedic@adobe.com>
2024-06-24 15:01:10 -0600

    -fix a comment and update timeout for propositions getter

--- AEPMessaging/Sources/Messaging+PublicAPI.swift
+++ AEPMessaging/Sources/Messaging+PublicAPI.swift
@@ -129,7 +129,7 @@ import UserNotifications
                           source: EventSource.requestContent,
                           data: eventData)
 
-        MobileCore.dispatch(event: event, timeout: 1) { responseEvent in
+        MobileCore.dispatch(event: event, timeout: 5) { responseEvent in
             guard let responseEvent = responseEvent else {
                 completion(nil, AEPError.callbackTimeout)
                 return
